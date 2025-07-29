c3ab7ac0c75c8fd5218e35699f2ee56e2076c3fd
Kairav01 <kairavjaiswal01@gmail.com>
2025-07-29 13:23:20 +0530

    New Program Added

--- Turtle 6.py
+++ Turtle 6.py
@@ -1,8 +1,12 @@
 import turtle
 
+wn = turtle.Screen()
+wn.bgcolor('black')
+
 squary = turtle.Turtle()
-squary.speed(100)
+squary.speed(10000000000000000000000000000000000)
+squary.color('green')
 
 for i in range(5000):
     squary.forward(i)
-    squary.left(110)
+    squary.left(91)
